ef90bf0995ba9aa728c1a32a3f83b2f09bc9d17f
Carol Romero <72485937+karoru23@users.noreply.github.com>
2024-04-29 09:52:21 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='WSI-PeruDB',
-    version='0.1',
+    name='WSIPeruDB',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -17,11 +17,12 @@ setup(
         'xlsxwriter'
     ],
     classifiers = [
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
     author='Carol Romero',
     author_email='romeroroldancarol@gmail.com',
-    description='This package allows users to access the Water Stable Isotope Database in Peru. It includes 464 stations over Peru, updated until 2023, and provides an interactive map for exploring the spatial distribution of all the stations. Additionally, it offers features for technical validation and display temporal series for each station and department across Peru',
+    description='Water Stable Isotope Database in Peru',
+    long_description='This package allows users to access the Water Stable Isotope Database in Peru. It includes 464 stations over Peru, updated until 2023, and provides an interactive map for exploring the spatial distribution of all the stations. Additionally, it offers features for technical validation and display temporal series for each station and department across Peru'
     url='https://github.com/karoru23/WSI-PeruDB',
 )
